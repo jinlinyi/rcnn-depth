c00da4a8b3df43a5df5e44d1820feb30bd6b2d7d
Andrew Price <arprice@gatech.edu>
2018-09-01 01:16:09 -0400

    Returning point cloud from HHA generation.

--- rcnn/saveHHA.m
+++ rcnn/saveHHA.m
@@ -1,4 +1,4 @@
-function HHA = saveHHA(imName, C, outDir, D, RD)
+function [HHA, HHA_pc] = saveHHA(imName, C, outDir, D, RD)
 % function HHA = saveHHA(imName, C, outDir, D, RD)
 
 % AUTORIGHTS
@@ -11,9 +11,9 @@ function HHA = saveHHA(imName, C, outDir, D, RD)
   [pc, N, yDir, h, pcRot, NRot] = processDepthImage(D*100, missingMask, C);
   angl = acosd(min(1,max(-1,sum(bsxfun(@times, N, reshape(yDir, 1, 1, 3)), 3))));
     
-  % Making the minimum depth to be 100, to prevent large values for disparity!!!
-  pc(:,:,3) = max(pc(:,:,3), 100); 
-  I(:,:,1) = 31000./pc(:,:,3); 
+  % Making the minimum depth to be 50, to prevent large values for disparity!!!
+  pc(:,:,3) = max(pc(:,:,3), 50); 
+  I(:,:,1) = 15000./pc(:,:,3);
   I(:,:,2) = h;
   I(:,:,3) = (angl+128-90); %Keeping some slack
   I = uint8(I);
@@ -22,4 +22,5 @@ function HHA = saveHHA(imName, C, outDir, D, RD)
   if(~isempty(outDir) && ~isempty(imName)), imwrite(I, fullfile_ext(outDir, imName, 'png')); end
   
   HHA = I;
+  HHA_pc=pc;
 end
